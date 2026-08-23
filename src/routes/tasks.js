8f1054a2e82e05547ca4f3239ba2646f159d07e2
harveym4n4lili <115873409+harveym4n4lili@users.noreply.github.com>
2026-08-23 21:02:00 +0100

    Stage 3: full CRUD on Postgres
    - updated DELETE, PUT and CREATE task by ID routes to be async and corrected their error handling
    - updated their corresponding service methods to connect with postgre

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -84,11 +84,11 @@ router.get('/', async (req, res) => {
  *       400:
  *         description: Invalid input
  */
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const newTask = req.body; // Get the new task data from the request body
 
     try {
-        const createResult = tasks.createTask(newTask); // Create a new task in the database and send it in the response
+        const createResult = await tasks.createTask(newTask); // Create a new task in the database and send it in the response
         res.status(201).send({ message: 'Task created successfully' }); // Send a success message with a 201 status 
     } catch (error) {
         res.status(400).send({ error: 'An error occurred while creating the task: ' + error.message }); // Send a 400 response with an error message if task creation fails
@@ -119,7 +119,8 @@ router.get('/:id', async (req, res) => {
         const findTaskResult = await tasks.getTaskById(taskId);
         res.status(200).send(findTaskResult); // Send the found task in the response
     } catch (error) {
-        res.status(404).send({ error: `Task ${taskId} not found` }); // Send a 404 response with an error message if the task is not found});
+        const statusCode = error.status || 500;
+        res.status(statusCode).send({ error: error.message }); // Send a error code response with an error message if the task is not found
     }
 }); // GET route to fetch a specific task by ID
 
@@ -140,21 +141,22 @@ router.get('/:id', async (req, res) => {
  *       404:
  *         description: Task not found
  */
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
     const taskId = req.params.id; // Get the task ID from the request parameters
 
     try {
-        const deleteTaskResult = tasks.deleteTaskById(taskId); //
+        const deleteTaskResult = await tasks.deleteTaskById(taskId);
         res.status(204).send(); // Send a 204 response indicating successful deletion
     } catch (error) {
-        res.status(404).send({ error: `Task ${taskId} not found` }); // Send a 404 response with an error message if the task is not found
+        const statusCode = error.status || 500;
+        res.status(statusCode).send({ error: error.message }); // Send a error code response with an error message if the task is not found
     }
 }); // DELETE route to delete a specific task by ID
 
 /**
  * @swagger
  * /tasks/{id}:
- *   patch:
+ *   put:
  *     summary: Update a task
  *     parameters:
  *       - in: path
@@ -181,16 +183,17 @@ router.delete('/:id', (req, res) => {
  *       404:
  *         description: Task not found
  */
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
     const taskId = req.params.id; // Get the task ID from the request parameters
 
     const { task_name, task_description, completed } = req.body; // Get the updated task data from the request body
 
     try {
-        const updateTaskResult = tasks.updateTaskById(taskId, { task_name, task_description, completed });
+        const updateTaskResult = await tasks.updateTaskById(taskId, { task_name, task_description, completed });
         res.status(200).send(updateTaskResult); // Send the updated task in the response
     } catch (error) {
-        res.status(404).send({ error: `Task ${taskId} not found` }); // Send a 404 response with an error message if the task is not found
+        const statusCode = error.status || 500;
+        res.status(statusCode).send({ error: error.message }); // Send a error code response with an error message if the task is not found
     }
 }); // PUT route to update a specific task by ID
 
--- src/services/tasks.js
+++ src/services/tasks.js
@@ -45,7 +45,7 @@ function validateCreate(task) {
     }
 }
 
-function createTask(task) {
+async function createTask(task) {
     validateCreate(task); // validate the task object before creating it
     const { task_name, task_description, completed} = task; // destructure the task object to get its properties
 
@@ -53,14 +53,14 @@ function createTask(task) {
 
     const isCompleted = completed ? 1 : 0; // convert the boolean completed value to an integer (1 for true, 0 for false)
 
-    const result = db.run(
-        'INSERT INTO tasks (task_name, task_description, completed, created_at, updated_at) VALUES (@task_name, @task_description, @completed, @created_at, @updated_at)',
-        {task_name, task_description, completed: isCompleted, created_at: currentTimestamp, updated_at: currentTimestamp}
+    const result = await pool.query(
+        'INSERT INTO tasks (task_name, task_description, completed, created_at, updated_at) VALUES ($1, $2, $3, $4, $5)',
+        [task_name, task_description, isCompleted, currentTimestamp, currentTimestamp]
     );
 
     let message = 'Error in creating task'; // default error message
 
-    if (result.changes) {
+    if (result.rowCount > 0) {
         message = 'Task created successfully'; // success message if the task was created
     }
 
@@ -79,7 +79,7 @@ async function getTaskById(id) {
     return { task: task.rows[0] }; // return the found task
 }
 
-function updatedTaskById(id, task) {
+async function updateTaskById(id, task) {
     validateCreate(task); // validate the task object before updating it
     const { task_name, task_description, completed } = task; // destructure the task object to get its properties
 
@@ -87,12 +87,12 @@ function updatedTaskById(id, task) {
 
     const isCompleted = completed ? 1 : 0; // convert the boolean completed value to an integer (1 for true, 0 for false)
 
-    const result = db.run(
-        'UPDATE tasks SET task_name = @task_name, task_description = @task_description, completed = @completed, updated_at = @updated_at WHERE id = @id',
-        { id, task_name, task_description, completed: isCompleted, updated_at: currentTimestamp }
+    const result = await pool.query(
+        'UPDATE tasks SET task_name = $1, task_description = $2, completed = $3, updated_at = $4 WHERE id = $5',
+        [task_name, task_description, isCompleted, currentTimestamp, id]
     );
 
-    if (!result.changes) {
+    if (result.rowCount === 0) {
         let error = new Error('Task not found'); // throw an error if the task is not found
         error.status = 404; // set the error status to 404 (Not Found)
         throw error;
@@ -101,10 +101,10 @@ function updatedTaskById(id, task) {
     return { message: 'Task updated successfully' }; // return a success message
 }
 
-function deleteTaskById(id) {
-    const result = db.run('DELETE FROM tasks WHERE id = ?', [id]); // delete the task with the given id from the database
+async function deleteTaskById(id) {
+    const result = await pool.query('DELETE FROM tasks WHERE id = $1', [id]); // delete the task with the given id from the database
 
-    if (!result.changes) {
+    if (result.rowCount === 0) {
         let error = new Error('Task not found'); // throw an error if the task is not found
         error.status = 404; // set the error status to 404 (Not Found)
         throw error;
@@ -114,6 +114,6 @@ export default {
     getAllTasks,
     getTaskById,
     deleteTaskById,
-    updatedTaskById,
+    updateTaskById,
     createTask
 }
